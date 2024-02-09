a7d8604eb0db003364ee0e8259b779ebabb3777a
bru1f0rc3 <wint1841@inbox.ru>
2024-02-09 14:40:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Sn(double x, int i = 6)
+            {
+                if (i < 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return i / Pow(x, i) + Sn(x, --i);
+                }
+            }
+            Console.Write("Введите число(x) ");
+            double X = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Ответ: {Sn(X):f3}");
+        }
+    }
+}
